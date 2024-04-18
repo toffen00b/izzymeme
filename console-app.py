b0050aa8fb2eb66ec19be52fa2bd6778688c6f56
toffen00b <97538507+toffen00b@users.noreply.github.com>
2024-04-18 11:20:47 +0500

    Add files via upload

---
+++ console-app.py
@@ -0,0 +1,10 @@
+print('HelloWorld! AIWACONF 1.1.0 | CODENAME: BLUEBERRY')
+print('Waiting for $ECHO, if you need help use $HELP')
+command1 = input('')
+text_o = "Optiization flags: --choco-on --defender-on"
+if command1 == help:
+    #confirm = print('Confirm the action! [Y]- YES, [N]-NO')
+    print(text_o)
+
+#if confirm == 'y':
+
